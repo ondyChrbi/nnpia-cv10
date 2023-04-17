e61770580e86834b4bcc90d1169c63411b38b562
ondyChrbi <ondrej.chrbolka@icloud.com>
2023-04-17 16:52:33 +0200

    2.	Seznamte se s React Query. a.	Nahraďte nově vytvořený hook řešením z React Query.

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Tasks from "./page/Tasks";
 import Header from "./component/ui/Header";
 import TaskDetail from "./page/TaskDetail";
+import {QueryClient, QueryClientProvider} from "react-query";
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
     <Provider store={store}>
-        <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route path={"/task"} element={<Tasks />} />
-                <Route path={"/task/:id"} element={<TaskDetail />} />
-            </Routes>
-        </BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path={"/task"} element={<Tasks />} />
+                    <Route path={"/task/:id"} element={<TaskDetail />} />
+                </Routes>
+            </BrowserRouter>
+        </QueryClientProvider>
     </Provider>
   )
 }
--- src/page/Tasks.tsx
+++ src/page/Tasks.tsx
@@ -2,17 +2,30 @@ import TaskList from "../component/TaskList";
 import './Tasks.css';
 import {useSelector} from "react-redux";
 import {RootState} from "../app/store";
-import {useFetchTasks} from "../features/task/hooks";
+import {useQuery} from "react-query";
+import {Task} from "../data/init-data";
 
 const Tasks = () => {
     const isLoggedIn = useSelector((state: RootState) => state.login.value);
-    const {tasks, loading, error} = useFetchTasks(isLoggedIn);
+    const {data, isError, error, isLoading} = useQuery("tasks", fetchData);
+
+    if (!isLoggedIn) {
+        return <div className="tasks">Sign in before showing Tasks</div>
+    }
 
     return <div className="tasks">
-        {error && <div className="alert alert-danger">{error}</div>}
-        {loading && <div className="alert alert-danger">loading</div>}
-        <TaskList tasks={tasks} />
+        {isError && <div className="alert alert-danger">{error as String}</div>}
+        {isLoading && <div className="alert alert-danger">loading</div>}
+        {data && <TaskList tasks={data as Array<Task>}/>}
     </div>
 };
+
+const fetchData = async () => {
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+    const result = await fetch(`${backendUrl}/task`);
+    return (await result.json()) as Array<Task>
+};
+
 
 export default Tasks;
