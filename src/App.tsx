31baf8b3914d323b742053285d4751b160c99858
ondyChrbi <ondrej.chrbolka@icloud.com>
2023-04-17 16:21:19 +0200

    Init projekt z předchozího cvičení

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+import {Provider} from "react-redux";
+import store from "./app/store";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Tasks from "./page/Tasks";
+import Header from "./component/ui/Header";
+import TaskDetail from "./page/TaskDetail";
+
+function App() {
+  return (
+    <Provider store={store}>
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path={"/task"} element={<Tasks />} />
+                <Route path={"/task/:id"} element={<TaskDetail />} />
+            </Routes>
+        </BrowserRouter>
+    </Provider>
+  )
+}
+
+export default App
